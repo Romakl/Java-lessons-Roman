e112de96d45b5934f5832b484c77f19d5ece4586
Romakl <80426376+Romakl@users.noreply.github.com>
2022-02-11 17:31:59 +0300

    Add files via upload

---
+++ src/com/company/Second_lesson.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Second_lesson {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        float first, second;
+        System.out.print("First num: ");
+        first = num.nextFloat();
+        if (first > 100 || first < 1)
+            System.out.println("Error: invalid Number");
+        else if (first >= 75)
+            System.out.println("Nice Job");
+        else if (first >= 26)
+            System.out.println("Lower average");
+        else if (first >= 1)
+            System.out.println("Try harder");
+        String[] names = {"John", "Bob", "Mary", "Peter", "Sarah"};
+        for (var i = 0; i < names.length; i++){
+            if (names[i].length()> 4) {
+                System.out.println("The name " + names[i] + " is longer than 4 letters");
+            } else if (names[i].length() < 4)
+                System.out.println("The name " + names[i] + " is shorter than 4 letters");
+            else if (names[i].length() == 4)
+                System.out.println("The name " + names[i] + " is 4 letters length");
+        }
+    }
+}
