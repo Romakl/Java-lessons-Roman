9bc449e8f79cce0c8cbd535b464e3ecb94c141e4
Romakl <romakl55555@gmail.com>
2022-04-17 00:05:36 +0300

    (file) Moved change file

---
+++ src/Test/Week7/BankMenuTest.java
@@ -0,0 +1,14 @@
+package Week7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankMenuTest {
+
+    @org.junit.jupiter.api.Test
+    void generateCardNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generateCVV() {
+    }
+}
