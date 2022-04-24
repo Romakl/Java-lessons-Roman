f05ae4f0fdb1dd3188e31d1c3e46db7728862f34
Romakl <romakl55555@gmail.com>
2022-04-24 04:20:44 +0300

    (feat) Added test, task 15 still in progress:(

--- src/Test/Week7/BankMenuTest.java
+++ src/Test/Week7/BankMenuTest.java
@@ -7,9 +7,16 @@ class BankMenuTest {
     //логика тестов не реализована
     @org.junit.jupiter.api.Test
     void generateCardNumber() {
+        String cardNumber = bankMenu.generateCardNumber();
+
+        assertEquals(12, cardNumber.length());
     }
 
     @org.junit.jupiter.api.Test
     void generateCVV() {
+        int CVV = bankMenu.generateCVV();
+        boolean expected = CVV >= 111 && CVV <= 999;
+
+        assertTrue(expected);
     }
 }
